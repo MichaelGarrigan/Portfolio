d5b9967276ce7508ca9689deb39b59373d09a609
M-garrigan <mgarrigan99@gmail.com>
2019-02-14 07:18:49 -0500

    The views dir now has a pages dir which will point to the complete 'page' to be rendered.
    Also the background is a svg which will be simplified and have ids and classes
    as hooks to animate and decorate the page.
    The svg will remain static and the other elements will be relative and placed on
    top of the svg.

--- main.js
+++ main.js
@@ -4,6 +4,11 @@ const app = express();
 app.use(express.static(__dirname + '/public'));
 app.set('view engine', 'ejs');
 
-app.get('/', (req, res) => res.render('index', {user: "michael"}));
+app.get('/', (req, res) => {
+  console.log('info: ', req.header('user-agent'))
+  console.log('req: ', req.headers)
+  console.log('ip: ', req.ip)
+  res.render('pages/index', {user: "michael"})
+});
 
 app.listen(3300, () => console.log('Express Server listening on port 3300'));

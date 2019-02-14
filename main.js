c7ca278b30464b18e0fbfe2c9c3af81ea058ce6f
M-garrigan <mgarrigan99@gmail.com>
2019-02-14 15:13:40 -0500

    Split the background svg into 2 parts -- the head and the body.
    The head can now be reused in multiple files.

--- main.js
+++ main.js
@@ -4,11 +4,16 @@ const app = express();
 app.use(express.static(__dirname + '/public'));
 app.set('view engine', 'ejs');
 
+
+
 app.get('/', (req, res) => {
-  console.log('info: ', req.header('user-agent'))
-  console.log('req: ', req.headers)
-  console.log('ip: ', req.ip)
+  // console.log('info: ', req.header('user-agent'))
+  // console.log('req: ', req.headers)
+  console.log('HOME');
   res.render('pages/index', {user: "michael"})
 });
+app.get('/blog', (req, res) => {
+  console.log('BLOG');
+  res.render('pages/blog')});
 
 app.listen(3300, () => console.log('Express Server listening on port 3300'));

97ee83308d35fb118500e049719e59c74710c551
M-garrigan <mgarrigan99@gmail.com>
2019-11-03 22:34:06 -0500

    Passing a variable to indicate which link is active

--- main.js
+++ main.js
@@ -5,10 +5,10 @@ const blogDict = require('./controllers/blogController.js');
 app.use(express.static(__dirname + '/public'));
 app.set('view engine', 'ejs');
 
-app.get('/', (req, res) => res.render('pages/index'));
-app.get('/applications', (req, res) => res.render('pages/applications'));
-app.get('/blog', (req, res) => res.render('pages/blog'));
-app.get('/resume', (req, res) => res.render('pages/resume'));
+app.get('/', (req, res) => res.render('pages/index', {link: 'home'}));
+app.get('/applications', (req, res) => res.render('pages/applications', {link: 'applications'}));
+app.get('/blog', (req, res) => res.render('pages/blog', {link: 'blog'}));
+app.get('/resume', (req, res) => res.render('pages/resume', {link: 'resume'}));
 app.get('/blog/:blog_name', (req, res) => {
   const name = req.params.blog_name;
 

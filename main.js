fc8484e0b0cd2ab8585523b7eeb84fa630e8d263
M-garrigan <mgarrigan99@gmail.com>
2019-12-01 14:19:40 -0500

    Small style adjustments and media query for over 1000px

--- main.js
+++ main.js
@@ -5,16 +5,35 @@ const blogDict = require('./controllers/blogController.js');
 app.use(express.static(__dirname + '/public'));
 app.set('view engine', 'ejs');
 
-app.get('/', (req, res) => res.render('pages/index', {link: 'home'}));
-app.get('/applications', (req, res) => res.render('pages/applications', {link: 'applications'}));
-app.get('/blog', (req, res) => res.render('pages/blog', {link: 'blog'}));
-app.get('/resume', (req, res) => res.render('pages/resume', {link: 'resume'}));
-app.get('/blog/:blog_name', (req, res) => {
-  const name = req.params.blog_name;
+app.get(
+  '/', 
+  (req, res) => res.render('pages/applications', {link: 'home'})
+);
 
-  if (blogDict[name])
-    res.render(`partials/blogs/${blogDict[name]}`)
-});
+app.get(
+  '/applications', 
+  (req, res) => res.render('pages/applications', {link: 'applications'})
+);
+
+app.get(
+  '/blog', 
+  (req, res) => res.render('pages/blog', {link: 'blog'})
+);
+
+app.get(
+  '/resume', 
+  (req, res) => res.render('pages/resume', {link: 'resume'})
+);
+
+app.get(
+  '/blog/:blog_name', 
+  (req, res) => {
+    const name = req.params.blog_name;
+
+    if (blogDict[name])
+      res.render(`partials/blogs/${blogDict[name]}`)
+  }
+);
 
 const port = process.env.PORT || 5080;
 
